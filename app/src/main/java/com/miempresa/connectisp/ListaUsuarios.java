08dc0902b4599a35bf536d523ba818c76bfbe8e8
Fabio Yela <flyela@estudiante.uniajc.edu.co>
2023-11-06 12:33:26 -0500

    Carga lista usuarios

---
+++ app/src/main/java/com/miempresa/connectisp/ListaUsuarios.java
@@ -0,0 +1,14 @@
+package com.miempresa.connectisp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaUsuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_usuarios);
+    }
+}
