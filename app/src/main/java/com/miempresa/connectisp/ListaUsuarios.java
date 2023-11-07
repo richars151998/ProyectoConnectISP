0f709a28b41f125f04a0fc81af7155321165048e
Flyela <flyela@estudiante.uniajc.edu.co>
2023-11-06 21:09:08 -0500

    Nuevo Diseño

--- app/src/main/java/com/miempresa/connectisp/ListaUsuarios.java
+++ app/src/main/java/com/miempresa/connectisp/ListaUsuarios.java
@@ -3,8 +3,10 @@ package com.miempresa.connectisp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ListaUsuarios extends AppCompatActivity {
 
@@ -16,5 +18,38 @@ public class ListaUsuarios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_usuarios);
+
+
+        TxtCodigo=findViewById(R.id.TxtCodigo);
+        TxtNombre=findViewById(R.id.TxtNombre);
+        TxtDireccion=findViewById(R.id.TxtDireccion);
+        TxtBarrio=findViewById(R.id.TxtBarrio);
+        BtnGuardar = findViewById(R.id.BtnGuardar);
+
+        BtnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+           // long id=  DbUsuarios dbUsuarios = new DbUsusarios(ListaUsuarios.this);
+             /*  DbUsuarios.isertarUsuarios(TxtCodigo.getText().toString(),
+                                          TxtNombre.getText().toString(),
+                                          TxtDireccion.getText().toString(),
+                                          TxtBarrio.getText().toString());*/
+
+             /*   if(id>0 ){
+                    Toast.makeText(ListaUsuarios.this, "REGISTRO GUARDADO",
+                                    Toast.LENGTH_LONG).show();
+                    Limpiar();
+                }else { Toast.makeText(ListaUsuarios.this, "ERROR AL GUARDAR REGISTRO",
+                        Toast.LENGTH_LONG).show();
+                }*/
+            }
+        });
+
+    }
+    private void Limpiar (){
+        TxtNombre.setText ( " " ) ;
+        TxtCodigo.setText ( " " ) ;
+        TxtDireccion.setText( " ") ;
+        TxtBarrio.setText(" ");
     }
 }
